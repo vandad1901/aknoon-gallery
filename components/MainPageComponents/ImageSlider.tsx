2dd6ab716758950e4eaf8dfad527faf8cf04425d
vandad1901 <vandad1901@gmail.com>
2023-12-07 02:04:43 +0330

    Fixed some main page content

--- components/MainPageComponents/ImageSlider.tsx
+++ components/MainPageComponents/ImageSlider.tsx
@@ -6,25 +6,28 @@ import slider3 from "@/public/slider-3.jpg";
 
 export default function ImageSlider() {
     return (
-        <div className="h-[70vh] w-full overflow-hidden bg-aknoon ">
+        <div className="h-[400px] w-full overflow-hidden bg-aknoon ">
             <div className="relative left-0 grid h-full w-[100vw] animate-slideShow grid-cols-3 justify-items-center transition-all">
-                <Image
-                    src={slider1}
-                    alt="placeholder"
-                    className="absolute left-0 mx-auto h-full w-full object-contain"
-                    priority={true}
-                />
+                <picture>
+                    <Image
+                        src={slider1}
+                        alt="placeholder"
+                        className="absolute left-0 mx-auto h-full w-full object-contain"
+                        height={400}
+                        priority={true}
+                    />
+                </picture>
                 <Image
                     src={slider2}
                     alt="placeholder"
+                    height={400}
                     className="absolute left-[100vw] mx-auto h-full w-full object-contain"
-                    priority={true}
                 />
                 <Image
                     src={slider3}
                     alt="placeholder"
+                    height={400}
                     className="absolute left-[200vw] mx-auto h-full w-full object-contain"
-                    priority={true}
                 />
                 <Image
                     src={slider1}
