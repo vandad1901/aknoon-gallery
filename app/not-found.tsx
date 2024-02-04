c95f029fbedc07c5c31c91af28a8b6e673db71e8
vandad1901 <vandad1901@gmail.com>
2024-02-04 06:51:45 +0330

    Fixed not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function NotFound() {
     return (
-        <div className="grid grid-cols-1 justify-items-center gap-1 pt-20">
+        <div className="grid grid-cols-1 justify-items-center gap-1 pt-40">
             <h2>صفحه مورد نظر پیدا نشد!</h2>
-            <Link href="/" className="text-aknoon hover:text-aknoon">
+            <Link href="/" className="text-sky-700">
                 بازگشت به صفحه اصلی
             </Link>
         </div>
