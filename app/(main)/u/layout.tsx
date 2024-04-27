4b09f9d741b22109002f4e8b95c7014edd52785f
vandad1901 <vandad1901@gmail.com>
2024-04-27 22:53:15 +0330

    added user page redirect

--- app/(main)/u/layout.tsx
+++ app/(main)/u/layout.tsx
@@ -1,7 +1,14 @@
+import { RedirectType, redirect } from "next/navigation";
+
 import Link from "next/link";
 import { profileItems } from "@/config/site";
+import { validateRequest } from "@/lib/authServices";
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+    const { session } = await validateRequest();
+    if (session == null) {
+        return redirect("/login", RedirectType.replace);
+    }
     return (
         <div className="flex flex-row gap-20 sm:px-20 sm:pt-10">
             <nav className="hidden h-min w-48 flex-col gap-3 rounded-xl border px-4 py-2 sm:flex">
