a42a6ffdf077503d038a967a73723fd8f07c0ec6
vandad1901 <vandad1901@gmail.com>
2025-12-07 15:31:52 +0330

    feat: migrated to turbopack

--- next.config.js
+++ next.config.js
@@ -10,10 +10,6 @@ const nextConfig = {
             },
         ],
     },
-    webpack: (config) => {
-        config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
-        return config;
-    },
 };
 
 module.exports = nextConfig;
